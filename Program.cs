1ea9e115d70a9fde36e63aeba958472736a47a18
profe <profedeinfocr@gmail.com>
2022-09-20 20:15:34 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progra2Clase2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1 = 20;
+            Console.WriteLine(num1);
+            Console.WriteLine(num1 *= 10);
+            Console.WriteLine(num1 /= 5);
+
+
+
+
+
+        }
+    }
+}
